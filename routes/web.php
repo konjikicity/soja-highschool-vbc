b82962680ae46cf70e13d5091ff9b3273470c7b9
syu.onoda <s-onoda@creative-innovation.co.jp>
2024-04-29 22:13:53 +0900

    fix: fix vercel

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,21 @@
 
 use App\Http\Controllers\Game\GameController;
 use App\Http\Controllers\HomeController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 
-Route::get('/', [HomeController::class, 'index'])->name('index');
+//Route::get('/', [HomeController::class, 'index'])->name('index');
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/games', [GameController::class, 'index'])->name('game.index');
