93afaca1a58e0972b8329bd29a35590aaf65790f
syu.onoda <s-onoda@creative-innovation.co.jp>
2024-05-03 19:02:02 +0900

    add: sidebar

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
 import NavLink from "@/Components/NavLink";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
+import { AnimatePresence, motion } from "framer-motion";
 import { Link } from "@inertiajs/react";
 import { useSearch } from "@/Context/SearchContext";
 
 export default function Authenticated({ children }) {
-    const [showingNavigationDropdown, setShowingNavigationDropdown] =
-        useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const { searchTerm, setSearchTerm } = useSearch();
+
+    const sidebarVariants = {
+        open: {
+            opacity: 1,
+            x: 0,
+            transition: { y: { stiffness: 1000 } },
+        },
+        closed: {
+            opacity: 0,
+            x: "-100%",
+            transition: { y: { stiffness: 1000 } },
+        },
+    };
 
     return (
         <div className="min-h-screen bg-gray-100">
@@ -64,12 +77,9 @@ export default function Authenticated({ children }) {
                                 placeholder="タイトルと日付で検索できます"
                                 className="w-[300px] px-4 py-2 border rounded-full"
                             />
+
                             <button
-                                onClick={() =>
-                                    setShowingNavigationDropdown(
-                                        (previousState) => !previousState,
-                                    )
-                                }
+                                onClick={() => setIsMenuOpen(!isMenuOpen)}
                                 className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                             >
                                 <svg
@@ -79,50 +89,58 @@ export default function Authenticated({ children }) {
                                     viewBox="0 0 24 24"
                                 >
                                     <path
-                                        className={
-                                            !showingNavigationDropdown
-                                                ? "inline-flex"
-                                                : "hidden"
-                                        }
+                                        className="inline-flex"
                                         strokeLinecap="round"
                                         strokeLinejoin="round"
                                         strokeWidth="2"
                                         d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                    <path
-                                        className={
-                                            showingNavigationDropdown
-                                                ? "inline-flex"
-                                                : "hidden"
-                                        }
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M6 18L18 6M6 6l12 12"
                                     />
                                 </svg>
                             </button>
                         </div>
                     </div>
                 </div>
-
-                <div
-                    className={
-                        (showingNavigationDropdown ? "block" : "hidden") +
-                        " sm:hidden"
-                    }
-                >
-                    <div className="pt-4 pb-1 border-t border-gray-200">
-                        <div className="mt-3 space-y-1">
-                            <ResponsiveNavLink
-                                method="post"
-                                href={route("logout")}
-                                as="button"
+                <div className="block sm:hidden">
+                    <AnimatePresence>
+                        {isMenuOpen && (
+                            <motion.div
+                                className="fixed inset-0 bg-black bg-opacity-50 z-50"
+                                initial="closed"
+                                animate="open"
+                                exit="closed"
+                                variants={sidebarVariants}
                             >
-                                ログアウト
-                            </ResponsiveNavLink>
-                        </div>
-                    </div>
+                                <motion.div className="h-screen bg-white relative">
+                                    <button
+                                        onClick={() => setIsMenuOpen(false)}
+                                        className="absolute top-4 right-4 p-2 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none"
+                                    >
+                                        <svg
+                                            className="h-6 w-6"
+                                            stroke="currentColor"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                        >
+                                            <path
+                                                className="black inline-flex"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M6 18L18 6M6 6l12 12"
+                                            />
+                                        </svg>
+                                    </button>
+                                    <ResponsiveNavLink
+                                        method="post"
+                                        href={route("logout")}
+                                        as="button"
+                                    >
+                                        ログアウト
+                                    </ResponsiveNavLink>
+                                </motion.div>
+                            </motion.div>
+                        )}
+                    </AnimatePresence>
                 </div>
             </nav>
 
