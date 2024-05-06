ae16d1f36840d825fc27ef1f8a8bf987d698baca
syu.onoda <s-onoda@creative-innovation.co.jp>
2024-05-06 10:19:44 +0900

    add: delete description

--- database/seeders/GameSeeder.php
+++ database/seeders/GameSeeder.php
@@ -13,10 +13,10 @@ class GameSeeder extends Seeder
     public function run(): void
     {
         $games = [
-            ['id' => 1, 'title' => '対天城高校①', 'description' => '', 'youtube_url' => 'https://www.youtube.com/watch?v=vE9CoQroDiw', 'game_date' => '2024-03-16', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 2, 'title' => '対天城高校③', 'description' => '', 'youtube_url' => 'https://www.youtube.com/watch?v=ugrbLrfx3-s', 'game_date' => '2024-03-16', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 3, 'title' => '対一宮高校①', 'description' => '', 'youtube_url' => 'https://www.youtube.com/watch?v=dpodSLGtRIA', 'game_date' => '2024-03-16', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 4, 'title' => '対一宮高校③', 'description' => '', 'youtube_url' => 'https://www.youtube.com/watch?v=PTx5z1iMJIs', 'game_date' => '2024-03-16', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 1, 'title' => '対天城高校①', 'youtube_url' => 'https://www.youtube.com/watch?v=vE9CoQroDiw', 'game_date' => '2024-03-16', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 2, 'title' => '対天城高校③', 'youtube_url' => 'https://www.youtube.com/watch?v=ugrbLrfx3-s', 'game_date' => '2024-03-16', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 3, 'title' => '対一宮高校①', 'youtube_url' => 'https://www.youtube.com/watch?v=dpodSLGtRIA', 'game_date' => '2024-03-16', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 4, 'title' => '対一宮高校③', 'youtube_url' => 'https://www.youtube.com/watch?v=PTx5z1iMJIs', 'game_date' => '2024-03-16', 'created_at' => now(), 'updated_at' => now()],
         ];
 
         DB::table('games')->insert($games);
